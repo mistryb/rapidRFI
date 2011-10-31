ea6f686b4da938181d54efcf1d82bbd8c5a4a80c
Bhavik Mistry <bhavik.mistry@gmail.com>
2011-10-30 22:00:19 -0400

    Initial Configurations Changed - Gii tool initialized(password cheese) - MySQL database initialized

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'rapidRFI',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -19,15 +19,15 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
+		// uncomment the following to enable the Gii tool(UNCOMMENTED)
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'cheese',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+		
 	),
 
 	// application components
@@ -50,16 +50,16 @@ return array(
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
-		// uncomment the following to use a MySQL database
-		/*
+		// uncomment the following to use a MySQL database(UNCOMMENTED)
+	
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=rapidrfi',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'cheese',
 			'charset' => 'utf8',
 		),
-		*/
+	
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
