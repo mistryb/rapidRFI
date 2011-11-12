9c9aee98e06508af2e80ead5108387286cce4aa8
Bhavik Mistry <bhavik.mistry@gmail.com>
2011-11-11 19:26:15 -0500

    Link to file on system added to the view

--- protected/views/rfi/_file.php
+++ protected/views/rfi/_file.php
@@ -1,7 +1,7 @@
 <div class="view">    
     
     <b><?php echo CHtml::encode($data->getAttributeLabel('filename')); ?>:</b>
-       <?php echo CHtml::encode($data->filename); ?>
+       <?php echo CHtml::link(CHtml::encode($data->filename), Yii::app()->baseUrl.'/uploads/'.$data->rfi_id."/".$data->filename); ?>
     <br />
         
 </div>
