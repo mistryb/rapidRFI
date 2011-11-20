5cc53837101c9a5ba81a5472b076c97cb8d5af59
Bhavik Mistry <bhavik.mistry@gmail.com>
2011-11-20 18:50:47 -0500

    Testing new Branch.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,7 +4,7 @@
     if(!Yii::app()->user->checkAccess('Authenticated'))
     {
         ?>
-        <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+        <h1>Welcome to Rapidfire <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
         <p> You may login using the link below:</p>
         <?php echo CHtml::link('Login', array('site/login'));
     }
